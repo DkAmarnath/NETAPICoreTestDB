0c81b39b35c1bd5eea3d0b5b99ac7ada09a3c1f1
DkAmarnath <34921125+DkAmarnath@users.noreply.github.com>
2020-11-23 10:34:25 +0530

    Add files via upload

---
+++ TestCoreWebAPI/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestCoreWebAPI.Models;
+
+namespace TestCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly TestDBContext _context;
+
+        public UserController(TestDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        [Route("GetUsers")]
+        public async Task<ActionResult<IEnumerable<Users>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
